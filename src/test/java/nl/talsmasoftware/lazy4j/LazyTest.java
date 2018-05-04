73244a0276e13ebf089158c7b9424129aaf480fa
Sjoerd Talsma <sjoerd@talsma-ict.nl>
2018-05-04 13:12:52 +0200

    Breaking change: rename package to 'lazy4j' in preparation for jigsaw

--- src/test/java/nl/talsmasoftware/lazy4j/LazyTest.java
+++ src/test/java/nl/talsmasoftware/lazy4j/LazyTest.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package nl.talsmasoftware.lazy;
+package nl.talsmasoftware.lazy4j;
 
 import org.junit.Before;
 import org.junit.Test;
