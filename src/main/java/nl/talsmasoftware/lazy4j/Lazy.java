326fded24793ae5ab9fec4505f45185da0854f12
Sjoerd Talsma <sjoerdtalsma@users.noreply.github.com>
2019-01-08 15:55:08 +0100

    Implement retry-on-exception mechanism (#20)

--- src/main/java/nl/talsmasoftware/lazy4j/Lazy.java
+++ src/main/java/nl/talsmasoftware/lazy4j/Lazy.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2018 Talsma ICT
+ * Copyright 2018-2019 Talsma ICT
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -34,6 +34,10 @@ import static java.util.Objects.requireNonNull;
  * <p>
  * {@code Lazy} objects are thread-safe. For every {@link Lazy} instance,
  * the supplier gets called either <em>never</em> or <em>once</em>.
+ * <p>
+ * There is one exception to this rule; if a {@code retryCount} is supplied (via {@linkplain #lazy(int, Supplier)}
+ * the value is re-evaluated until the retry counter reaches zero.
+ * {@linkplain Integer#MAX_VALUE} is interpreted as infinite retries.
  *
  * @author Sjoerd Talsma
  */
@@ -42,9 +46,18 @@ public final class Lazy<T> implements Supplier<T> {
     private volatile Supplier<T> supplier;
     private volatile T result;
     private volatile RuntimeException exception;
+    private int retryCount;
 
-    private Lazy(Supplier<T> supplier) {
+    /**
+     * Constructor for unevaluated lazy object.
+     *
+     * @param retryCount The maximum number of retries in case of exceptions
+     *                   (Only if &gt;= 0. Unlimited if {@code Integer.MAX_VALUE})
+     * @param supplier   The supplier (required)
+     */
+    private Lazy(int retryCount, Supplier<T> supplier) {
         this.supplier = requireNonNull(supplier, "Lazy function is <null>.");
+        this.retryCount = retryCount;
     }
 
     /**
@@ -60,8 +73,28 @@ public final class Lazy<T> implements Supplier<T> {
      * @return A lazy placeholder for the supplier result that will only obtain it when needed.
      */
     public static <T> Lazy<T> lazy(Supplier<T> supplier) {
-        return new Lazy<>(supplier);
+        return lazy(0, supplier);
     }
+
+    /**
+     * Create a {@linkplain Lazy} object that calls the specified {@code supplier}
+     * only when the lazy value is needed for the first time.
+     * From then on, the result is re-used by the lazy instance.
+     * <p>
+     * {@code Lazy} objects are thread-safe. For every {@link Lazy} instance,
+     * the supplier gets called either <em>never</em> or <em>once</em> unless the supplier throws an exception.
+     * In case of exceptions, the supplier is retried until the retry counter reaches zero.
+     *
+     * @param maxRetryCount The maximum number of supplier calls to make before the exception becomes 'permanent'.
+     *                      Set to {@code Integer.MAX_VALUE} to keep delegating to the supplier until a result is obtained.
+     * @param supplier      The value supplier for the lazy placeholder
+     * @param <T>           The type of the lazy value
+     * @return A lazy placeholder for the supplier result that will only obtain it when needed.
+     */
+    public static <T> Lazy<T> lazy(int maxRetryCount, Supplier<T> supplier) {
+        return new Lazy<>(maxRetryCount, supplier);
+    }
+
 
     /**
      * Eagerly evaluates the lazy supplier (at most once)
@@ -79,7 +112,8 @@ public final class Lazy<T> implements Supplier<T> {
                     } catch (RuntimeException supplierException) {
                         result = null;
                         exception = supplierException;
-                        supplier = null;
+                        if (retryCount <= 1) supplier = null;
+                        else if (retryCount != Integer.MAX_VALUE) retryCount--;
                     }
                 }
             }
