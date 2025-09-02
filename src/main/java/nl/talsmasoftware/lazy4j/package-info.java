f8eaaa1db54d5bc3efa6927be181609d6121f6da
Sjoerd Talsma <sjoerdtalsma@users.noreply.github.com>
2025-09-02 20:50:38 +0200

    Javadoc: Extend package documentation with LazyList and LazyValueMap.
    Signed-off-by: Sjoerd Talsma <sjoerdtalsma@users.noreply.github.com>

--- src/main/java/nl/talsmasoftware/lazy4j/package-info.java
+++ src/main/java/nl/talsmasoftware/lazy4j/package-info.java
@@ -14,15 +14,27 @@
  * limitations under the License.
  */
 /**
- * A generic {@link Lazy} class for java.
+ * A generic {@link nl.talsmasoftware.lazy4j.Lazy} class for java.
+ *
  * <p>
- * Java 8 defines a generic {@code Optional} type and a {@code Supplier} function for use with lambda's,
- * but unfortunately, there is no such thing as a 'lazy supplier function that re-uses the result'.
+ * Java 8 defines a generic {@link java.util.Optional} type and a {@link java.util.function.Supplier} function
+ * for use with lambda's, but unfortunately, there is no such thing as
+ * a 'lazy supplier function that re-uses the result'.
+ *
  * <p>
  * This is often useful for expensive operations that need to be performed at most once,
  * only if they are actually needed.
  * Declaring a 'lazy supplier' function is a common pattern for this
  * and can be easily defined with the introduction of Lambda's in Java 8.
+ *
+ * <h2>Lazy collections</h2>
+ * The following collection implementations are provided containing lazy values:
+ * <ul>
+ *     <li>{@link nl.talsmasoftware.lazy4j.LazyList} - a {@link java.util.List} of lazy values,
+ *     behaving like a regular list.
+ *     <li>{@link nl.talsmasoftware.lazy4j.LazyValueMap} - a {@link java.util.Map} of lazy values,
+ *     behaving like a regular map.
+ * </ul>
  *
  * @author Sjoerd Talsma
  */
