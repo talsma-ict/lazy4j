86b8ecedfadecf344ac47c065d91ce4c20cf849c
Sjoerd Talsma <sjoerdtalsma@users.noreply.github.com>
2026-01-17 15:33:05 +0100

    Bump maven wrapper and update copyright year.
    Signed-off-by: Sjoerd Talsma <sjoerdtalsma@users.noreply.github.com>

--- src/main/java/nl/talsmasoftware/lazy4j/LazyList.java
+++ src/main/java/nl/talsmasoftware/lazy4j/LazyList.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2018-2025 Talsma ICT
+ * Copyright 2018-2026 Talsma ICT
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
